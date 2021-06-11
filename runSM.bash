07f1ad5393235dc1d83ee6124715f2ffde5479a1
abdallahamr <abdelmaksoudaa@nnih.gov>
2021-06-11 04:35:49 -0400

    fix conda

--- runSM.bash
+++ runSM.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+conda env create -f workflow/envs/environment_telescope.yml --force
+source /data/$USER/conda/etc/profile.d/conda.sh
+conda activate telescope_env
+
 module load snakemake/5.5.2 
 
 snakemake  -s workflow/Snakefile --printshellcmds -j 50 --rerun-incomplete --keep-going --cluster "sbatch  --cpus-per-task 24 -t 120:00:00 --mem 96g"
