ced998b726dffc15bd9ce20e45445ee2c540c5ba
vinegang <vinegang@users.noreply.github.com>
2022-07-25 11:26:19 -0400

    updated the cluster resources and added containers to the rules

--- runSM.bash
+++ runSM.bash
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-conda env create -f workflow/envs/environment_telescope.yml --force
-source /data/$USER/conda/etc/profile.d/conda.sh
-conda activate telescope_env
 
 module load snakemake/5.5.2 
-
-snakemake  -s workflow/Snakefile --printshellcmds -j 50 --rerun-incomplete --keep-going --cluster "sbatch  --cpus-per-task 24 -t 120:00:00 --mem 96g"
+module load singularity
+snakemake --use-singularity --singularity-args " -B /data/khanlab/ "  -s workflow/Snakefile --printshellcmds -j 50 --rerun-incomplete --keep-going --cluster "sbatch  --cpus-per-task 24 -t 120:00:00 --mem 96g" --directory "/data/khanlab/projects/singularity_erv" --configfile "/data/khanlab/projects/testing_ERV_pipeline/ervTelescope/config/config.yaml"
 
 
